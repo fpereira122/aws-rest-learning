f2fcf8ab4639d08dddd8ddf9dbf00526e52acc84
fpereira122 <108684881+fpereira122@users.noreply.github.com>
2022-09-26 10:30:25 +0100

    Initial push - auth done (amplify)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "basicauthproject5f83dad8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
